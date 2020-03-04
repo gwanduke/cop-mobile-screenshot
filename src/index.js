e1401d6b2fe9bc570ec1e8640bcf40fa3d5e1f2c
gwanduke <letsget23@gmail.com>
2020-03-05 01:19:59 +0900

    Support nested folder

--- src/index.js
+++ src/index.js
@@ -1,13 +1,61 @@
 const sharp = require("sharp");
 const fs = require("fs");
 const path = require("path");
+const { close, open, utimes } = require("fs");
 
-fs.readdir(path.join(__dirname, "../images/source"), function(err, files) {
-  if (err) {
-    return console.log("Unable to scan directory: " + err);
-  }
+var walk = function(dir, done) {
+  var results = [];
+  fs.readdir(dir, function(err, list) {
+    if (err) return done(err);
+    var i = 0;
+    (function next() {
+      var file = list[i++];
+      if (!file) return done(null, results);
+      file = path.resolve(dir, file);
+      fs.stat(file, function(err, stat) {
+        if (stat && stat.isDirectory()) {
+          walk(file, function(err, res) {
+            results = results.concat(res);
+            next();
+          });
+        } else {
+          results.push(file);
+          next();
+        }
+      });
+    })();
+  });
+};
 
-  files.forEach(filename => {
+const touch = (path, callback) => {
+  const time = new Date();
+  utimes(path, time, time, err => {
+    if (err) {
+      return open(path, "w", (err, fd) => {
+        err ? callback(err) : close(fd, callback);
+      });
+    }
+    callback();
+  });
+};
+
+walk(path.join(__dirname, "../images/source"), function(err, result) {
+  result.forEach(sourcePath => {
+    const pathes = sourcePath.split("/");
+    const filename = pathes[pathes.length - 1];
+    const resultPath = sourcePath.replace("images/source", "images/result");
+    const resultFolder = resultPath
+      .split("/")
+      .reduce((acc, cur, idx, arr) => {
+        if (arr.length - 1 !== idx) acc.push(cur);
+
+        return acc;
+      }, [])
+      .join("/");
+    fs.mkdirSync(resultFolder, { recursive: true });
+
+    // const fd = fs.openSync(resultPath, "w+");
+
     if (/\.(gif|jpe?g|tiff|png|webp|bmp)$/i.test(filename)) {
       const transformer = sharp()
         .extract({
@@ -20,14 +68,14 @@ fs.readdir(path.join(__dirname, "../images/source"), function(err, files) {
           console.log("Image height is " + info.height);
         });
 
-      const readStream = fs.createReadStream(
-        path.join(__dirname, "../images/source", filename)
-      );
-      const writeStream = fs.createWriteStream(
-        path.join(__dirname, "../images/result", filename)
-      );
+      const readStream = fs.createReadStream(sourcePath);
+      // var mtime = parseInt(new Date().getTime() / 1000);
+      // fs.futimesSync(fd, mtime, mtime);
 
+      // touch(resultPath, () => {
+      const writeStream = fs.createWriteStream(resultPath);
       readStream.pipe(transformer).pipe(writeStream);
+      // });
     }
   });
 });
